41e36afdbe93165584106584c41f904953dc0c65
savage_for_code <34770707+savageF@users.noreply.github.com>
2019-05-14 01:08:01 +0900

    Update login.php

--- login.php
+++ login.php
@@ -1,33 +1,30 @@
+<?php
 
-<?php 
- 
-$host="localhost";
-$user="root";
-$password="";
-$db="demo";
- 
-mysql_connect($host,$user,$password);
-mysql_select_db($db);
- 
-if(isset($_POST['username'])){
-    
-    $uname=$_POST['username'];
-    $password=$_POST['password'];
-    
-    $sql="select * from loginform where user='".$uname."'AND Pass='".$password."' limit 1";
-    
-    $result=mysql_query($sql);
-    
-    if(mysql_num_rows($result)==1){
-        echo " You Have Successfully Logged in";
-        exit();
-    }
-    else{
-        echo " You Have Entered Incorrect Password";
-        exit();
-    }
-        
+session_start(); // 세션
+include ("connect.php"); // DB접속
+
+
+
+$id = $_POST['id']; // 아이디 
+$pw = $_POST['pw']; // 패스워드
+   
+$query = "select * from users where id='$id' and pw='$pw'";
+$result = mysqli_query($con, $query); 
+$row = mysqli_fetch_array($result);
+
+
+
+if($id==$row['id'] && $pw==$row['pw']){ // id와 pw가 맞다면 login
+
+   $_SESSION['id']=$row['id'];
+   $_SESSION['name']=$row['name'];
+   echo "<script>location.href='login.php';</script>";
+
+}else{ // id 또는 pw가 다르다면 login 폼으로
+
+   echo "<script>window.alert('invalid username or password');</script>"; // 잘못된 아이디 또는 비빌번호 입니다
+   echo "<script>location.href='login.php';</script>";
+
 }
+
 ?>
-
-
